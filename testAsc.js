0bb1d1e9b5242155bb55f37a3a717efe09c3e21f
Jeremie Cyrille <jeremie.cyrille@icloud.com>
2020-12-10 17:17:41 +0100

    call ready insidecall not define

--- testAsc.js
+++ testAsc.js
@@ -1,12 +1,21 @@
-const UP= 1;
-const DOWN= -1;
-const IDLE=0;
+const UP= 1;const DOWN= -1;const IDLE=0;
+
+function goUp(){
+  console.log("elevator up");
+}
+
+function goDown(param) {
+  console.log("elevator up");
+}
 
 function call(floors,direction){ 
     this.floors= floors;
     this.direction= direction;     
 }
-
+function button(level){ 
+  this.level= level;
+    
+}
 listCall=[
     new call(1,DOWN),
     new call(5,UP),
@@ -14,27 +23,48 @@ listCall=[
     new call(1,DOWN),
     new call(10,DOWN)   
 ];
-function upAsc(){
+
+function upAsc(ascFloor){
+  console.log("test");
+}
+function DownAsc(ascFloor){
+
+  console.log("test");
 
 }
-function DownAsc(){
-    
+
+function inElevator(lvElevator){
+const insideCall1= 5;
+const insideCall2= 6;
+
+
+
 }
 function idleCoreAsc(listCall){
-    const initAsc = new call(0,IDLE);
-    if (listCall.isArray(array) && array.length) {
-        setTimeout(function() { idleCoreAsc(); }, 5000);  
-        console.log("waiting"); 
-    }else{
-        console.log("calls receive:/n");
-        listCall.forEach(element => {
-          if(listCall.floors>initAsc.floors){
-            upAsc();
-          }else{
-            DownAsc();
-          } 
-        });
-        
-    }
-    
+  const initAsc = new call(0,IDLE);
+  if (listCall==null && listCall.length==0) {
+      setTimeout(function() { idleCoreAsc(); }, 5000);  
+      console.log("waiting..."); 
+  }else{
+    console.log("Calls receive!");
+    for (let i = 0; i < listCall.length; i++) {
+      if(listCall[i].floors>initAsc.floors){
+        console.log("going to level.....");
+        while(initAsc.floors!=listCall[i].floors){
+          goUp();
+          initAsc.floors++;
+        }
+        console.log("Done!\nPickup client.....");
+        inElevator(initAsc.floors);
+      }else if(listCall[i].floors<initAsc.floors){
+        while(listCall[i].floors!=initAsc.floors){
+          goDown();
+            initAsc.floors--;
+          }
+        console.log("Done!\nPickup client.....")
+        inElevator(initAsc.floors);
+      } 
+    }       
+  }  
 }
+idleCoreAsc(listCall);
